4a5b6082e67cdf0bf0bc905e9ee1e73941f21920
Nicolas Fontan <nico.fontan1@gmail.com>
2020-09-21 15:12:24 -0400

    Homework start up: basic html for the page, a function for the submit button, and 2 functions for getting the objects for the current weather and 5 day forecast respectively

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
-alert("yes");
+$(document).ready(function ()
+{
+    $("#weather-btn").on("click", function(event)
+    {
+        event.preventDefault();
+        
+        var cityInput = $("#weather-input").val().trim();
+        currentWeather(cityInput);
+        forecast(cityInput);
+    });
+
+    function currentWeather(city)
+    {
+        $.ajax(
+        {
+            method: "GET",
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=a0770a518a887ec5aa5cb207e7a24651"
+        }).then(function(data)
+        {
+            console.log(data);
+        })
+    }
+
+    function forecast(city)
+    {
+        $.ajax(
+        {
+            method: "GET",
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=a0770a518a887ec5aa5cb207e7a24651"
+        }).then(function(data)
+        {
+            console.log(data);
+        })
+    }
+
+});
