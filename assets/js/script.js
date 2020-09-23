4474480319712224e3cc7794436aaa91493144df
Nicolas Fontan <nico.fontan1@gmail.com>
2020-09-22 22:03:47 -0400

    Finished the appending of text for the user's city search to Current Weather, except for the UV index.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,28 @@
 // Global Variables
+var currentCityEl = $("<h2>");
+var currentTempEl = $("<p>");
+var currentHumidityEl = $("<p>");
+var currentWindEl = $("<p>");
+var uvIndexEl = $("<p>");
+var currentImage = $("<img>");
+
+var forecastDivEl = $("<div>");
+var forecastCardEl = $("<div>");
+var forecastDateEl = $("<h5>");
+var forecastImageEl = $("<img>");
+var forecastTempEl = $("<p>");
+var forecastHumidityEl = $("<p>");
+
+var forecastDiv = $("#forecast");
+var currentWeatherDiv = $("#currentWeather");
+
+//Setting up the elements to have the appropriate classes for styling
+forecastDivEl.addClass("card text-white bg-primary mb-3 mr-4");
+forecastDivEl.css("max-width", "10rem");
+forecastCardEl.addClass("card-body");
+forecastDateEl.addClass("card-title");
+forecastTempEl.addClass("card-text");
+forecastHumidityEl.addClass("card-text");
 
 
 $(document).ready(function ()
@@ -24,6 +48,15 @@ $(document).ready(function ()
             var lat = data.coord.lat;
             var lon = data.coord.lon;
             uvIndex(lat,lon);
+
+            currentCityEl.text(data.name + " (" + moment().format("MM/DD/YYYY") + ")");
+            currentTempEl.text("Temperature: " + data.main.temp + " \u00B0F");
+            currentHumidityEl.text("Humidity: " + data.main.humidity + "%");
+            currentWindEl.text("Wind Speed: " + data.wind.speed + " MPH");
+
+            currentCityEl.append($("<img>",{src:"http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"}));
+
+            currentWeatherDiv.append(currentCityEl, currentTempEl, currentHumidityEl, currentWindEl);
 
         })
     }
